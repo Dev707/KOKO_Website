28ad391e6911c304c12751049fe443173fc22c27
Khalid Alghamdi <52568487+Dev707@users.noreply.github.com>
2021-11-29 22:56:53 +0300

    validator fixed

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,10 @@
 <div class="footer" style="margin-top: 20px;">
     <?php
+    echo "<a href='http://validator.w3.org/check?uri=koko.ksg.sa'><img
+      src='http://www.w3.org/Icons/valid-xhtml10' alt='Valid XHTML 1.0 Transitional' height='31' width='88' /></a>";
     echo "<a href='https://jigsaw.w3.org/css-validator/check/referer'>
-    <img style='border:0;width:88px;height:31px'
-        src='https://jigsaw.w3.org/css-validator/images/vcss'
-        alt='Valid CSS!' />
-    </a>";
-    echo "<a href='http://jigsaw.w3.org/css-validator/check/referer'>
         <img style='border:0;width:88px;height:31px'
-            src='http://jigsaw.w3.org/css-validator/images/vcss-blue'
+            src='https://jigsaw.w3.org/css-validator/images/vcss-blue'
             alt='Valid CSS!' />
         </a>";
     echo "<a style='display:block; padding:0px;'>© KSG.sa</a>";
