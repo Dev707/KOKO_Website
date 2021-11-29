ea83553e51628ac8cac57c418a73f6dd51b4cad0
Khalid Alghamdi <52568487+Dev707@users.noreply.github.com>
2021-11-29 22:29:16 +0300

    css/xhtml validation

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,5 @@
 <div class="footer" style="margin-top: 20px;">
     <?php
-    $validator = "https://jigsaw.w3.org/css-validator/";
-    $valImg = "images/validator.png";
-    $valImg2 = "images/validator2.png";
-    $validator2 = "http://www.w3.org/Icons/valid-xhtml10";
     echo "<a href='https://jigsaw.w3.org/css-validator/check/referer'>
     <img style='border:0;width:88px;height:31px'
         src='https://jigsaw.w3.org/css-validator/images/vcss'
